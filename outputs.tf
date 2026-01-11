bbb506503d420722ff13b580698bf99f796f4c63
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:37:42 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "synapse_sql_pool_workload_groups" {
+  description = "All synapse_sql_pool_workload_group resources"
+  value       = azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups
+}
+
