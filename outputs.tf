40eda1a70e9f66efbbd173f72ad0fccca8cce5af
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "synapse_sql_pool_workload_groups" {
-  description = "All synapse_sql_pool_workload_group resources"
-  value       = azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups
-}
 output "synapse_sql_pool_workload_groups_importance" {
-  description = "List of importance values across all synapse_sql_pool_workload_groups"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : v.importance]
+  description = "Map of importance values across all synapse_sql_pool_workload_groups, keyed the same as var.synapse_sql_pool_workload_groups"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : k => v.importance }
 }
 output "synapse_sql_pool_workload_groups_max_resource_percent" {
-  description = "List of max_resource_percent values across all synapse_sql_pool_workload_groups"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : v.max_resource_percent]
+  description = "Map of max_resource_percent values across all synapse_sql_pool_workload_groups, keyed the same as var.synapse_sql_pool_workload_groups"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : k => v.max_resource_percent }
 }
 output "synapse_sql_pool_workload_groups_max_resource_percent_per_request" {
-  description = "List of max_resource_percent_per_request values across all synapse_sql_pool_workload_groups"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : v.max_resource_percent_per_request]
+  description = "Map of max_resource_percent_per_request values across all synapse_sql_pool_workload_groups, keyed the same as var.synapse_sql_pool_workload_groups"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : k => v.max_resource_percent_per_request }
 }
 output "synapse_sql_pool_workload_groups_min_resource_percent" {
-  description = "List of min_resource_percent values across all synapse_sql_pool_workload_groups"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : v.min_resource_percent]
+  description = "Map of min_resource_percent values across all synapse_sql_pool_workload_groups, keyed the same as var.synapse_sql_pool_workload_groups"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : k => v.min_resource_percent }
 }
 output "synapse_sql_pool_workload_groups_min_resource_percent_per_request" {
-  description = "List of min_resource_percent_per_request values across all synapse_sql_pool_workload_groups"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : v.min_resource_percent_per_request]
+  description = "Map of min_resource_percent_per_request values across all synapse_sql_pool_workload_groups, keyed the same as var.synapse_sql_pool_workload_groups"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : k => v.min_resource_percent_per_request }
 }
 output "synapse_sql_pool_workload_groups_name" {
-  description = "List of name values across all synapse_sql_pool_workload_groups"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : v.name]
+  description = "Map of name values across all synapse_sql_pool_workload_groups, keyed the same as var.synapse_sql_pool_workload_groups"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : k => v.name }
 }
 output "synapse_sql_pool_workload_groups_query_execution_timeout_in_seconds" {
-  description = "List of query_execution_timeout_in_seconds values across all synapse_sql_pool_workload_groups"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : v.query_execution_timeout_in_seconds]
+  description = "Map of query_execution_timeout_in_seconds values across all synapse_sql_pool_workload_groups, keyed the same as var.synapse_sql_pool_workload_groups"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : k => v.query_execution_timeout_in_seconds }
 }
 output "synapse_sql_pool_workload_groups_sql_pool_id" {
-  description = "List of sql_pool_id values across all synapse_sql_pool_workload_groups"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : v.sql_pool_id]
+  description = "Map of sql_pool_id values across all synapse_sql_pool_workload_groups, keyed the same as var.synapse_sql_pool_workload_groups"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_group.synapse_sql_pool_workload_groups : k => v.sql_pool_id }
 }
 
